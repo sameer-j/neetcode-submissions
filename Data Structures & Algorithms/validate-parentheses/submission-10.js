e496888f6bcc1b23a009f5ee557118381e4af269
Sameer Jain <samir4nov@gmail.com>
2026-05-07 08:40:00 +0530

    Add: validate-parentheses - submission-10

---
+++ Data Structures & Algorithms/validate-parentheses/submission-10.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        let stack = [];
+        let bracket_map = {
+            '(' : ')',
+            '[' : ']',
+            '{' : '}'
+        };
+
+        for(let c of s) {
+            if(c in bracket_map) {
+                stack.push(c);
+            }
+            else {
+                if(stack.length === 0 || bracket_map[stack[stack.length -1]] != c) {
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+        return stack.length == 0;
+    }
+}
